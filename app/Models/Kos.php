9a279ea976afbcdfbb68079e916eb2343b6e0c1f
dimas-aulia <dimaz.rizki2017@gmail.com>
2026-06-21 22:05:52 +0700

    Menambahkan

--- app/Models/Kos.php
+++ app/Models/Kos.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 //firdi
 //hafid
+
+// pantek
 class Kos extends Model
 {
     protected $table = 'kos';
