ebfb60bb6b0a029c22a85b356a0fc688dc01b3a2
252-MrSumar <2400018252@webmail.uad.ac.id>
2026-06-21 22:16:36 +0700

    m

--- app/Models/Kos.php
+++ app/Models/Kos.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 //hafid
 class Kos extends Model
 {
+    //firdi
     protected $table = 'kos';
     protected $fillable = [
         'user_id',
